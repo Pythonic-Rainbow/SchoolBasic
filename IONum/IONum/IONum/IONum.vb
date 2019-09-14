175effe36302a7e9d67f58afa4e532c00e18c09c
Pythonic-Rainbow <wongchingho0@gmail.com>
2019-09-14 21:38:31 +0100

    IONum

---
+++ IONum/IONum/IONum/IONum.vb
@@ -0,0 +1,10 @@
+﻿Module IONum
+
+    Sub Main()
+        Console.Write("Please enter number ")
+        Dim input As Integer = Console.ReadLine()
+        Console.WriteLine("The number is " & input)
+        Console.ReadLine()
+    End Sub
+
+End Module
